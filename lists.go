26880fa0ebff0b7ab26a1b819b117c5cda21dcd4
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-18 15:32:04 +0100

    Print elements with particular indexes from arrays

--- lists.go
+++ lists.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string
+	productNames = [4]string{"A book", "A T-shirt", "A notebook", "A desktop"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
-	fmt.Println(prices)
+	fmt.Println(prices[1])
+	fmt.Println(productNames[1])
 }
