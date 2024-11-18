05d09ddc989a489d4603ce54288e63d43f351ce6
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-18 15:51:15 +0100

    Use list slice

--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module alevitt.com/go.lists_and_arrays
+module alevitt.com/go.lists
--- lists.go
+++ lists.go
@@ -8,4 +8,8 @@ func main() {
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices[1])
 	fmt.Println(productNames[1])
+
+	// First element in slices is included, second is excluded
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
 }
