e344cf40e62b454a21dd2f85a91aca5bd9c2abe5
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-20 17:45:53 +0100

    Dynamic arrays and slicing

--- lists.go
+++ lists.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+/*func main() {
 	var productNames [4]string
 	productNames = [4]string{"A book", "A T-shirt", "A notebook", "A desktop"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
@@ -12,4 +12,13 @@ func main() {
 	// First element in slices is included, second is excluded
 	featuredPrices := prices[1:3]
 	fmt.Println(featuredPrices)
+}*/
+
+func main() {
+	prices := []float64{10.99, 8.99}
+	prices[1] = 9.99
+	newArr := prices[1:2]
+	fmt.Println(newArr)
+
+	fmt.Println(append(newArr, 5.99), prices)
 }
