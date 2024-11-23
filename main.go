f3b0c145c7046d938f2297edfaed679f7a2b4e1e
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-23 17:07:07 +0100

    Use make to create map

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ func main() {
 	userNames := make([]string, 2, 5) // type, empty slots, capacity(~max size)
 	userNames[0] = "Jenifer"
 	userNames[1] = "Julia"
-
 	userNames = append(userNames, "John", "Jane")
-
 	fmt.Println(userNames)
+
+	courseRatings := make(map[string]float64, 3) // type, capacity
+	courseRatings["Go"] = 4.7
+	courseRatings["React"] = 4.8
+	courseRatings["Vue"] = 4.7
+	fmt.Println(courseRatings)
 }
