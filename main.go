f790574ebeeed43ab78ff09fddf48d4604f227e3
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-23 17:25:02 +0100

    Use type aliases

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	userNames := make([]string, 2, 5) // type, empty slots, capacity(~max size)
 	userNames[0] = "Jenifer"
@@ -9,9 +15,10 @@ func main() {
 	userNames = append(userNames, "John", "Jane")
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3) // type, capacity
+	courseRatings := make(floatMap, 3) // type, capacity
 	courseRatings["Go"] = 4.7
 	courseRatings["React"] = 4.8
 	courseRatings["Vue"] = 4.7
-	fmt.Println(courseRatings)
+	courseRatings.output()
+	// fmt.Println(courseRatings)
 }
